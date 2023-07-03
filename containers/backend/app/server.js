7b4c84c691feaa77ca30c96f1ac43b60ed5dbee6
ChristianP01 <christianpreti01@gmail.com>
2023-07-03 18:26:38 +0200

    testing di invio immagini al backend

--- containers/backend/app/server.js
+++ containers/backend/app/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const app = express();
 
+
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
@@ -16,14 +17,32 @@ const upload = multer({
 });
 const port = 8080;
 
-app.post('/upload', upload.single('picture'), (req, res) => {
+app.post('/test', upload.single('picture'), (req, res) => {
     res.send("Richiesta POST effettuata.")
     console.log(req.file);
 
     res.sendStatus(200);
 })
 
-app.get('/upload', (req, res) => {
+app.post('/imageupload', upload.single('image'), (req, res) => {
+    const { name, image } = req.body;
+    const binaryString = atob(image);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; ++i) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    const blob = new Blob([bytes], { type: 'image/png' });
+    createImageBitmap(blob).then(bitmap => {
+      // fai qualcosa con l'oggetto Bitmap qui...
+      res.sendStatus(200);
+    }).catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+  });
+
+app.get('/test', (req, res) => {
     res.send("Richiesta GET effettuata.")
 
     res.sendStatus(200);
--- mobile/app/src/main/java/com/example/locsnap/MainActivity.kt
+++ mobile/app/src/main/java/com/example/locsnap/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.locsnap
-
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Base64
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var jsonObjectRequest: JsonObjectRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,29 +29,62 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.my_main)
 
         val buttonConnect = findViewById<Button>(R.id.connectButton)
-
         val textView = findViewById<TextView>(R.id.textView)
 
-        buttonConnect.setOnClickListener {
-            val queue = Volley.newRequestQueue(this)
-            val url = getString(R.string.base_url) + "/upload"
+        val queue = Volley.newRequestQueue(this@MainActivity)
+        
+        val chooseFile = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
 
-            val stringRequest = StringRequest(
-                Request.Method.GET,
-                url,
-                { response ->
-                    // Handle the response
-                    textView.text = response
-                },
-                { error ->
-                    // Handle the error
-                    textView.text = error.message
-                }
-            )
+            val inputStream = uri?.let { contentResolver.openInputStream(it) }
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            uploadImage(bitmap, queue)
 
-            queue.add(stringRequest)
+            /*val cursor = uri?.let { contentResolver.query(it, null, null, null, null) }
+            cursor?.use {
+                it.moveToFirst()
+                val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                val fileName = it.getString(nameIndex)
+                textView.text = fileName
+            }*/
+
         }
 
+        buttonConnect.setOnClickListener {
+            // Let user choose photo to be uploaded
+            chooseFile.launch("image/*")
+        }
+    }
+
+    private fun uploadImage(bitmap: Bitmap?, queue: RequestQueue) {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val image: String = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT)
+        val name: String = java.lang.String.valueOf(Calendar.getInstance().getTimeInMillis())
+        try {
+            val url = getString(R.string.base_url) + "/imageupload"
+            val jsonObject = JSONObject()
+            jsonObject.put("name", name)
+            jsonObject.put("image", image)
+            jsonObjectRequest = JsonObjectRequest(
+                Request.Method.POST, url, jsonObject,
+                { response ->
+                    try {
+                        val message = response.getString("message")
+                        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                        Toast.makeText(this@MainActivity, "ERRORE", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            ) {
+                Toast.makeText(this@MainActivity, "Image upload failed.", Toast.LENGTH_SHORT).show()
+                val textView = findViewById<TextView>(R.id.textView)
+                textView.text = it.message
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        queue.add(jsonObjectRequest)
     }
 
 }
