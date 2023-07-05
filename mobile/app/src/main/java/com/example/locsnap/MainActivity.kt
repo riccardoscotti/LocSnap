aebbd0086e99cfc4dcff6afe8bce0661c9d5fcab
ChristianP01 <christianpreti01@gmail.com>
2023-07-05 17:54:45 +0200

    risoluzione conflitti

--- mobile/app/src/main/java/com/example/locsnap/MainActivity.kt
+++ mobile/app/src/main/java/com/example/locsnap/MainActivity.kt
@@ -11,20 +11,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 import java.io.ByteArrayOutputStream
-<<<<<<< Updated upstream
-import java.util.*
-=======
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
->>>>>>> Stashed changes
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,30 +30,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.my_main)
 
         val buttonConnect = findViewById<Button>(R.id.connectButton)
-        val textView = findViewById<TextView>(R.id.textView)
-
         val queue = Volley.newRequestQueue(this@MainActivity)
         
         val chooseFile = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-<<<<<<< Updated upstream
-=======
-            val queue = Volley.newRequestQueue(this@MainActivity)
             userLogin("ChristianP01", "christian123", queue)
->>>>>>> Stashed changes
 
             val inputStream = uri?.let { contentResolver.openInputStream(it) }
             val bitmap = BitmapFactory.decodeStream(inputStream)
 
             uploadImage(bitmap, queue)
-
-            /*val cursor = uri?.let { contentResolver.query(it, null, null, null, null) }
-            cursor?.use {
-                it.moveToFirst()
-                val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                val fileName = it.getString(nameIndex)
-                textView.text = fileName
-            }*/
-
         }
 
         buttonConnect.setOnClickListener {
@@ -67,8 +47,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-<<<<<<< Updated upstream
-=======
     private fun encrypt(pass: String, key: String): String {
         val iv = "1234567890123456".toByteArray(charset("utf-8"))
         val skeySpec = SecretKeySpec(key.toByteArray(charset("utf-8")), "AES")
@@ -79,7 +57,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun userLogin(username: String, password: String, queue: RequestQueue) {
-        val enc_key = "locsnap-project-enckey2023"
+        //val enc_key = "locsnap-project-enckey2023"
         //val enc_pwd = encrypt(password, enc_key)
 
         val jsonObject = JSONObject()
@@ -111,25 +89,22 @@ class MainActivity : AppCompatActivity() {
      * @param bitmap, the bitmap that needs to be sent to backend.
      * @param queue, the requests queue containing them
      */
->>>>>>> Stashed changes
     private fun uploadImage(bitmap: Bitmap?, queue: RequestQueue) {
-        /*val byteArrayOutputStream = ByteArrayOutputStream()
-        val textView = findViewById<TextView>(R.id.textView)
-        bitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
-        val image: String = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT)*/
-        //Toast.makeText(this@MainActivity, image.length.toString(), Toast.LENGTH_SHORT).show()
         val byteArrayOutputStream = ByteArrayOutputStream()
         bitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
         val image: String = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT)
         val url = getString(R.string.base_url) + "/imageupload"
-        val name: String = java.lang.String.valueOf(Calendar.getInstance().getTimeInMillis())
+        val currentDateTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")
+        val formattedDateTime = currentDateTime.format(formatter)
+        val name: String = "IMG_" + formattedDateTime
 
         val jsonObject = JSONObject()
         jsonObject.put("name", name)
         jsonObject.put("image", image)
 
         val jsonObjectRequest = object : JsonObjectRequest(
-            Request.Method.POST, url, jsonObject,
+            Method.POST, url, jsonObject,
             { response ->
                 if (response.getString("status").equals("200"))
                     Toast.makeText(this@MainActivity, "Image successfully sent.", Toast.LENGTH_SHORT).show()
@@ -138,16 +113,12 @@ class MainActivity : AppCompatActivity() {
             },
             {
                 Toast.makeText(this@MainActivity, "Image upload failed.", Toast.LENGTH_SHORT).show()
-                val textView = findViewById<TextView>(R.id.textView)
-                textView.text = it.message
             }
         ) {
             override fun getBodyContentType(): String {
                 return "application/json; charset=utf-8"
             }
         }
-
         queue.add(jsonObjectRequest)
     }
-
 }
