e0995ef33b2300560305680409ee9427e375be80
ChristianP01 <christianpreti01@gmail.com>
2023-07-31 16:09:23 +0200

    Fragment implementati

--- mobile/app/src/main/java/com/example/locsnap/MainActivity.kt
+++ mobile/app/src/main/java/com/example/locsnap/MainActivity.kt
@@ -1,99 +1,33 @@
 package com.example.locsnap
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
 import android.util.Base64
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
-import com.google.android.material.switchmaterial.SwitchMaterial
 import org.json.JSONObject
 import java.io.ByteArrayOutputStream
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Contenuto del file layout/activity_main.xml
+        setContentView(R.layout.my_activity_main)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
         val navController = findNavController(R.id.my_content_main)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        // Contenuto del file layout/login_scren.xml
-        setContentView(R.layout.fragment_login)
-
-        val loginButton = findViewById<Button>(R.id.loginButton)
-        val switchScreen = findViewById<SwitchMaterial>(R.id.switchScreen) // Allows to switch between register/login mode.
-        val username = findViewById<EditText>(R.id.usernameText)
-        val password = findViewById<EditText>(R.id.passwordText)
-        val queue = Volley.newRequestQueue(this@MainActivity)
-        
-        val chooseFile = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-            val inputStream = uri?.let { contentResolver.openInputStream(it) }
-            val bitmap = BitmapFactory.decodeStream(inputStream)
-            uploadImage(bitmap, queue)
-        }
-
-        // Continue on register mode
-        switchScreen.setOnCheckedChangeListener { _, isChecked ->
-            if(!isChecked) {
-                // ...
-            }
-        }
-
-        loginButton.setOnClickListener {
-            userLogin(username.text.toString(), password.text.toString(), queue)
-            // TODO Move chooseFile... to another ClickListener, getting it activated by another button.
-            //chooseFile.launch("image/*") // Open file chooser
-        }
-    }
-
-    /**
-     * Simple password hashing using SHA-256 algorithm.
-     * @param password: User password as-is, not hashed.
-     * @return password's SHA-256.
-     */
-    fun hashPassword(password: String): String {
-        val bytes = password.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        return digest.fold("", { str, it -> str + "%02x".format(it) })
-    }
-
-    private fun userLogin(username: String, password: String, queue: RequestQueue) {
-        val jsonObject = JSONObject()
-        jsonObject.put("username", username)
-        jsonObject.put("password", hashPassword(password))
-
-        val loginRequest = JsonObjectRequest(
-            Request.Method.POST,
-            getString(R.string.base_url) + "/login",
-            jsonObject,
-            { response ->
-                if (response.getString("status").equals("200"))
-                    Toast.makeText(this@MainActivity, "Login successful", Toast.LENGTH_SHORT).show()
-                else if (response.getString("status").equals("401"))
-                    Toast.makeText(this@MainActivity, "Login error.", Toast.LENGTH_SHORT).show()
-            },
-            {
-                Toast.makeText(this@MainActivity, "[LOGIN] Communication error.", Toast.LENGTH_SHORT).show()
-            }
-        )
-        queue.add(loginRequest)
     }
 
     /**
--- mobile/app/src/main/java/com/example/locsnap/RegisterFragment.kt
+++ mobile/app/src/main/java/com/example/locsnap/RegisterFragment.kt
@@ -5,40 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.locsnap.databinding.FragmentFirstBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class RegisterFragment : Fragment() {
-
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
-
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        val switchScreen = view.findViewById<SwitchMaterial>(R.id.switchScreen)
+        // Continue on login mode
+        switchScreen.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                findNavController().navigate(R.id.action_register_to_login)
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
