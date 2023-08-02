3cdff02220c6d992614542f9576bf8e2926d4f90
ChristianP01 <christianpreti01@gmail.com>
2023-08-02 21:52:46 +0200

    interfaccia v1.0

--- mobile/app/src/main/java/com/example/locsnap/ChooseFragment.kt
+++ mobile/app/src/main/java/com/example/locsnap/ChooseFragment.kt
@@ -1,5 +1,6 @@
 package com.example.locsnap
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -11,11 +12,13 @@ import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 
 class ChooseFragment : Fragment() {
 
     private lateinit var input : String
     private lateinit var pickMultipleMedia: ActivityResultLauncher<PickVisualMediaRequest>
+    private lateinit var pickSingleMedia: ActivityResultLauncher<PickVisualMediaRequest>
     private var maxPhotos : Int = 10
 
     override fun onCreateView(
@@ -35,6 +38,18 @@ class ChooseFragment : Fragment() {
             }
         }
 
+        pickSingleMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    Log.d("PhotoPicker", "Selected URI: $uri")
+                    //val inputStream = requireActivity().contentResolver.openInputStream(uri)?.close()
+                    // val bytes = inputStream?.readBytes()
+
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
+
         return inflater.inflate(R.layout.fragment_choose, container, false)
     }
 
@@ -43,12 +58,37 @@ class ChooseFragment : Fragment() {
 
         val welcomeText = view.findViewById<TextView>(R.id.welcomeText)
         val uploadButton = view.findViewById<Button>(R.id.uploadButton)
+        val newCollectionButton = view.findViewById<Button>(R.id.collectionButton)
 
-        welcomeText.text = input
+        welcomeText.text = "${welcomeText.text} $input!"
 
         uploadButton.setOnClickListener {
-            // Launch the photo picker and let the user choose only images.
+
+            // User needs to choose to upload either a single image or an already-created collection.
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Choose an option")
+                .setItems(arrayOf("Single photo", "Existing collection"),
+                    DialogInterface.OnClickListener { dialog, which ->
+                        when(which) {
+                            0 -> { // Single photo
+                                // Launch the photo picker allowing user to select only one image.
+                                pickSingleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                            }
+
+                            1 -> { // Existing collection
+                                // How to retrieve existing collections?
+                            }
+                        }
+                    })
+
+            builder.create().show()
+        }
+
+        newCollectionButton.setOnClickListener {
+            // Launch the photo picker allowing the user to select more images.
             pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
+
+
     }
 }
