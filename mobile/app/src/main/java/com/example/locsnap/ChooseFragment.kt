c8fd9a4bac60b32a08a9b15b83e4f913dd1c6cb9
ChristianP01 <christianpreti01@gmail.com>
2023-08-02 11:30:40 +0200

    implementazione photo-picker

--- mobile/app/src/main/java/com/example/locsnap/ChooseFragment.kt
+++ mobile/app/src/main/java/com/example/locsnap/ChooseFragment.kt
@@ -1,16 +1,22 @@
 package com.example.locsnap
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 
 class ChooseFragment : Fragment() {
 
     private lateinit var input : String
+    private lateinit var pickMultipleMedia: ActivityResultLauncher<PickVisualMediaRequest>
+    private var maxPhotos : Int = 10
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,15 +26,29 @@ class ChooseFragment : Fragment() {
         val args = this.arguments
         input = args?.getString("loggedUsername").toString()
 
+        pickMultipleMedia =
+        registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(maxPhotos)) { uris ->
+            if (uris.isNotEmpty()) {
+                Log.d("PhotoPicker", "Number of items selected: ${uris.size}")
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
         return inflater.inflate(R.layout.fragment_choose, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
         super.onViewCreated(view, savedInstanceState)
 
         val welcomeText = view.findViewById<TextView>(R.id.welcomeText)
+        val uploadButton = view.findViewById<Button>(R.id.uploadButton)
 
         welcomeText.text = input
+
+        uploadButton.setOnClickListener {
+            // Launch the photo picker and let the user choose only images.
+            pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
     }
 }
