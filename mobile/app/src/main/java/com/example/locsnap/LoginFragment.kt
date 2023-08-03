074a8d6ee26222b84bf647c0bd85db16296b5893
ChristianP01 <christianpreti01@gmail.com>
2023-08-03 11:17:54 +0200

    creazione utils per separare la logica dall'ui

--- mobile/app/src/main/java/com/example/locsnap/LoginFragment.kt
+++ mobile/app/src/main/java/com/example/locsnap/LoginFragment.kt
@@ -10,13 +10,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.findNavController
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.material.switchmaterial.SwitchMaterial
-import org.json.JSONObject
-import java.security.MessageDigest
 
 class LoginFragment : Fragment() {
 
@@ -44,56 +39,7 @@ class LoginFragment : Fragment() {
         }
 
         loginButton.setOnClickListener {
-            userLogin(username.text.toString(), password.text.toString(), queue)
+            UserAuthenticate.userLogin(username.text.toString(), password.text.toString(), queue, this)
         }
-    }
-
-    /**
-     * Simple password hashing using SHA-256 algorithm.
-     * @param password: User password as-is, not hashed.
-     * @return password's SHA-256.
-     */
-    fun hashPassword(password: String): String {
-        val bytes = password.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        return digest.fold("", { str, it -> str + "%02x".format(it) })
-    }
-
-    private fun userLogin(username: String, password: String, queue: RequestQueue) {
-        val jsonObject = JSONObject()
-        jsonObject.put("username", username)
-        jsonObject.put("password", hashPassword(password))
-
-        val loginRequest = JsonObjectRequest(
-            Request.Method.POST,
-            getString(R.string.base_url) + "/login",
-            jsonObject,
-            { response ->
-                if (response.getString("status").equals("200")) {
-
-                    val bundle = Bundle()
-                    bundle.putString("loggedUsername", username)
-                    val fragment = ChooseFragment()
-                    fragment.arguments = bundle
-
-                    //val actualFragment = activity?.supportFragmentManager?.findFragmentById(this.id)
-
-                    val fragmentTransaction: FragmentTransaction? = activity?.supportFragmentManager?.beginTransaction()
-                    fragmentTransaction?.addToBackStack("LocSnap") // It allows to go back to previous screen
-                    fragmentTransaction?.remove(this)?.replace(R.id.app_container, fragment)
-                    fragmentTransaction?.commit()
-
-                    // If login's successful, go to chooseMenu
-                    //findNavController().navigate(R.id.action_login_to_choose)
-                } else if (response.getString("status").equals("401")) {
-                    Toast.makeText(this.context, "Login error.", Toast.LENGTH_SHORT).show()
-                }
-            },
-            {
-                Toast.makeText(this.context, "[LOGIN] Communication error.", Toast.LENGTH_SHORT).show()
-            }
-        )
-        queue.add(loginRequest)
     }
 }
